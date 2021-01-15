77cd9822cbef7ebf75c23bf8b65271db87f9e5aa
Shlok-Tandon <75730167+Shlok-Tandon@users.noreply.github.com>
2021-01-15 19:48:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+const World = Matter.World;
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 300;
+
+function preload() {
+
+  bg = loadImage("PLINKO BG.png");
+}
+
+function setup() {
+  createCanvas(500,800);
+
+  engine = Engine.create();
+  world = engine.world
+  
+  
+  ground = new Ground();
+  
+
+  for (var k = 5; k <= width; k = k + 81.75) {
+    divisions.push(new Division(k, height - divisionHeight/2, 7, divisionHeight));
+  }
+
+  for (var j = 40;j <= width; j = j + 50) {
+    plinkos.push(new Plinko(j,75));
+}
+
+for (var j = 15; j <= width - 10; j = j+50) {
+    plinkos.push(new Plinko(j,175));
+}
+
+for (var j = 40; j <= width; j = j + 50) {
+   plinkos.push(new Plinko(j,275));
+}
+
+for (var j = 15;j <= width - 10; j = j+50) {
+  plinkos.push(new Plinko(j,375));
+}
+
+
+
+  
+  
+}
+
+function draw() {
+  background(bg);
+  Engine.update(engine);  
+  ground.display();
+ 
+  for ( var i = 0; i < plinkos.length; i++) {
+  
+    plinkos[i].display();
+  }
+
+  if(frameCount%20 === 0) {
+    particles.push(new Particle(random(width/2-10, width/2+10), 10, 10));
+    
+  }
+
+  for (var j = 0; j < particles.length; j++) {
+    particles[j].display();
+  }
+
+  for (var k = 0; k < divisions.length;k++) {
+    divisions[k].display();
+  }
+  
+  
+}
